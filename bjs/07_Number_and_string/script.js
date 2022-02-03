4e8b7f1a089fbc98eac3410c265c311a5fccc3bf
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-02-03 13:46:03 +0200

    refresh

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,12 +1,22 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#type');
 
 
-document.getElementById('btn_clr').addEventListener('click', function () {
+
+
+document.querySelector('.numpad').addEventListener('click', function () {
+if(inputWindow.textContent == "") {
+    inputWindow.textContent = text;
+}
+else if (inputWindow.textContent.length <=26){
+    inputWindow.textContent = inputWindow.textContent + text;
+}
+})
+
+document.querySelector('#clear').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
-})
-
+}) 
