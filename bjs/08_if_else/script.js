fc0c4d5180f1d471a206e40a04f4065193d82f37
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-02-23 06:15:02 +0200

    last

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,39 +1,18 @@
 let minValue = parseInt(prompt('Minimal number to play','0'));
 let maxValue = parseInt(prompt('Maximum number to play','100'));
-if ((minValue != Number) || (maxValue != Number)) {
-minValue = 0;
-maxValue = 100;
-}
-alert(`Geas one number from ${minValue} till ${maxValue},and i will geas it`);
-let answerNumber  = Math.floor(minValue + maxValue) /2;
-
-function NumInWords (number) {
-    const first = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-    const tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-    const mad = ['', 'thousand', 'million', 'billion', 'trillion'];
-    let word = '';
-  
-    for (let i = 0; i < mad.length; i++) {
-      let tempNumber = number%(100*Math.pow(1000,i));
-      if (Math.floor(tempNumber/Math.pow(1000,i)) !== 0) {
-        if (Math.floor(tempNumber/Math.pow(1000,i)) < 20) {
-          word = first[Math.floor(tempNumber/Math.pow(1000,i))] + mad[i] + ' ' + word;
-        } else {
-          word = tens[Math.floor(tempNumber/(10*Math.pow(1000,i)))] + '-' + first[Math.floor(tempNumber/Math.pow(1000,i))%10] + mad[i] + ' ' + word;
-        }
-      }
-  
-      tempNumber = number%(Math.pow(1000,i+1));
-      if (Math.floor(tempNumber/(100*Math.pow(1000,i))) !== 0) word = first[Math.floor(tempNumber/(100*Math.pow(1000,i)))] + 'hunderd ' + word;
-    }
-      return word;
-  }
+alert(`Geas any number from ${minValue} till ${maxValue},and i will geas it`);
+let answerNumber  = Math.floor(minValue + maxValue) / 2;
 let orderNumber = 1; 
 let gameRun = true;
 const orderNumberField = document.querySelector('#orderNumberField');
 const answerField = document.querySelector('#answerField');
-orderNumberField.textContent= orderNumber;
-answerField.te= `You geas number  ${answerNumber}?`;
+orderNumberField.textContent = orderNumber;
+answerField.textContent = `You geas number  ${answerNumber}?`;
+if ((minValue != Number) || (maxValue != Number)) {
+    minValue = 0 ; 
+    maxValue - 100;
+};
+
 
 
 
@@ -46,8 +25,8 @@ document.querySelector('#btnOver').addEventListener('click', function () {
         if (minValue === maxValue){
             const phraseRandom = Math.round( Math.random()*2);
             const answerPhrase = (phraseRandom === 1) ?
-                `I gueas wrong number!\n\u{1F914}` :
-                `Geas some less number! \n\u{1F92F}`;
+                `I gueas wrong number!\u{1F97A}` :
+                `Geas some less number! \u{1F62D}`;
 
             answerField.textContent = answerPhrase;
             gameRun = false;
@@ -56,12 +35,10 @@ document.querySelector('#btnOver').addEventListener('click', function () {
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            const phraseRandom = Math.round( Math.random()*2);
-            const answerPhrase = (phraseRandom === 1) ?
-                `You geas less?\n\u{1F914}`:
-                'Try other number?\n\u{1F914}';
-                `Try less?\n\u{1F92F}`;
-            answerField.textContent = answerPhrase + answerNumber;
+                const phrases = [`You geas less?\u{1F928}`,
+                'Try other number?\u{1F644}',
+                'Try again?\n\u{1F914}'];
+            answerField.textContent = phrases[Math.floor(Math.random()*phrases.length)] + answerNumber;
         }
     }
 })
@@ -71,34 +48,27 @@ document.querySelector('#btnLess').addEventListener('click', function () {
         if (minValue === maxValue){
             const phraseRandom = Math.round( Math.random()*2);
             const answerPhrase = (phraseRandom === 1) ?
-                `I gueas wrong number!\n\u{1F914}`:
-                `I give up..\n\u{1F92F}`;
+                `I gueas wrong number!\u{1F97A}`:
+                `I give up..\u{1F62D}!`;
 
-            answerField.innerText = answerPhrase;
+            answerField.textContent = answerPhrase;
             gameRun = false;
     } else {
             minValue = answerNumber  - 1;
-            answerNumber  = Math.floor((minValue - maxValue) / 2);
-            maxValue++;
+            answerNumber = Math.floor((minValue - maxValue) / 2);
             orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            const phraseRandom = Math.round( Math.random()*2);
-            const answerPhrase = (phraseRandom === 1) ?
-                `You need bigger number?\n\u{1F914}`:
-                'Amost try again higher ?\n\u{1F914}';
-                `Try more\n\u{1F92F}`;
-            answerField.textContent = answerPhrase + answerNumber;
+            orderNumberField.textContent = orderNumber;
+            const phrases = ['You need bigger number?\u{1F9D0}',
+            'Amost try again higher ? \u{1F615}',
+            'Try again?\u{1F913}'];
+            answerField.textContent = phrases[Math.floor(Math.random()*phrases.length)] + answerNumber;
     }
 }
 })
 document.querySelector('#btnEqual').addEventListener('click', function () {
     if (gameRun){
-        const phraseRandom = Math.round( Math.random() * 3);
-        const answerPhrase = (phraseRandom === 3) ?
-                     `I aways win\n\u{1F60E}`:
-                     `I just Won \u{1F973}`;
-                     'That was win lets play again';
-        answerField.innerText = answerPhrase
+        const phrases =['I aways win\n\u{1F60E}','I just Won \u{1F60D}','That was win lets play again \u{1F973}'];
+        answerField.textContent = phrases[Math.floor(Math.random()* phrases.length)];
         gameRun = false;
     }
 })
