fe623db904e04e32ff75b7dd37dde8f2f590c2b1
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-03-09 20:03:11 +0200

    again

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -6,5 +6,6 @@ window.onload = function()
     document.querySelector('surnameOutput').textContent = initPerson.surName;
     document.querySelector('genderOutput').textContent = initPerson.gender;
     document.querySelector('birthYearOutput').textContent = initPerson.randomDate;
+    document.querySelector('birthYearOutput').textContent = initPerson.randomDate;
 };
 
